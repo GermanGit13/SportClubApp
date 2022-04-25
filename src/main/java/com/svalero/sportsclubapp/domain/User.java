28dd3b9a4de85b791f9d2c785348ef73477afea3
GermanGit13 <99618445+GermanGit13@users.noreply.github.com>
2022-04-25 11:52:04 +0200

    Clase Usuario

--- src/main/java/com/svalero/sportsclubapp/domain/User.java
+++ src/main/java/com/svalero/sportsclubapp/domain/User.java
@@ -1,4 +1,15 @@
 package com.svalero.sportsclubapp.domain;
 
+import java.time.LocalDate;
+
 public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate birthDate;
+    private String dni;
+    private String username;
+    private String password;
+    private boolean coach;
 }
